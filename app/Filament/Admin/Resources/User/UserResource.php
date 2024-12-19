4f766933a198559a5f1018cf14bf2f069da14cdd
Syawal Achmad <syawalachmad1323@gmail.com>
2024-12-19 16:22:27 +0800

    Refactor: create new directory for readable

---
+++ app/Filament/Admin/Resources/User/UserResource.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Filament\Admin\Resources\User;
+
+use App\Filament\Admin\Resources\User\UserResource\Pages;
+use App\Filament\Admin\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = 'User';
+
+    protected static ?string $navigationGroup = 'User';
+
+    protected static ?int $navigationsort = 2;
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('role', '!=', '0');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('username')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('password')
+                    ->translateLabel()
+                    ->password()
+                    ->dehydrateStateUsing(fn($state) => Hash::make($state))
+                    ->dehydrated(fn($state) => \filled($state))
+                    ->required(fn(string $context): bool => $context === 'create'),
+                Forms\Components\Select::make('staff')
+                    ->options([
+                        'pers' => 'Staf Tuud/Pers',
+                        'sikomlek' => 'Sikomlek',
+                        'pernika' => 'Staf Pernika',
+                        'konbekharstal' => 'Konbekharstal',
+                        'benghubdam' => 'Benghubdam',
+                        'gudmathub' => 'Gudmathub',
+                        'urlog' => 'Urlog',
+                        'urlat' => 'Urlat',
+                        'urpam' => 'Urpam',
+                        'renproggar' => 'Renproggar',
+                        'denhubdam' => 'Denhubdam'
+                    ])
+                    ->columnSpanFull()
+                    ->native(false)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('username')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('role')
+                    ->searchable()
+                    ->formatStateUsing(fn($state) => match ($state) {
+                        '1' => 'Admin',
+                        '2' => 'Staff',
+                    }),
+                Tables\Columns\TextColumn::make('staff')
+                    ->searchable()
+                    ->formatStateUsing(fn($state) => match ($state) {
+                        'pers' => 'Staf Tuud/Pers',
+                        'sikomlek' => 'Sikomlek',
+                        'pernika' => 'Staf Pernika',
+                        'konbekharstal' => 'Konbekharstal',
+                        'benghubdam' => 'Benghubdam',
+                        'gudmathub' => 'Gudmathub',
+                        'urlog' => 'Urlog',
+                        'urlat' => 'Urlat',
+                        'urpam' => 'Urpam',
+                        'renproggar' => 'Renproggar',
+                        'denhubdam' => 'Denhubdam'
+                    })
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('role')
+                    ->native(false)
+                    ->options([
+                        '1' => 'Admin',
+                        '2' => 'Staff',
+                    ])
+                    ->label('Role'),
+                Tables\Filters\SelectFilter::make('staff')
+                    ->native(false)
+                    ->options([
+                        'pers' => 'Staf Tuud/Pers',
+                        'sikomlek' => 'Sikomlek',
+                        'pernika' => 'Staf Pernika',
+                        'konbekharstal' => 'Konbekharstal',
+                        'benghubdam' => 'Benghubdam',
+                        'gudmathub' => 'Gudmathub',
+                        'urlog' => 'Urlog',
+                        'urlat' => 'Urlat',
+                        'urpam' => 'Urpam',
+                        'renproggar' => 'Renproggar',
+                        'denhubdam' => 'Denhubdam'
+                    ])
+                    ->label('Staf')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+        ];
+    }
+}
