f2cdeb34f8bd4e965dff92240c6cf7ccd0b8e2f5
Syawal Achmad <syawalachmad1323@gmail.com>
2024-12-22 16:44:34 +0800

    fix: minor

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center gap-4">
-    <img src="{{ asset('assets/images/logo.png') }}" alt="logo claro" class="h-10 w-9">
+    <img src="{{ asset('assets/images/logo.png') }}" alt="logo" class="h-10 w-9">
     <h1 class="font-bold text-2xl italic">{{ env('APP_NAME') }}</h1>
 </div>
