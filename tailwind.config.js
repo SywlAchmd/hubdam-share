74922addc0767cb3e2cbac8aab70bdbaffa62816
Syawal Achmad <syawalachmad1323@gmail.com>
2024-12-25 01:10:58 +0800

    feat: setup inertiajs for react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,32 @@
 import defaultTheme from "tailwindcss/defaultTheme";
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/**/*.blade.php",
-        "./resources/views/**/*.blade.php",
-        "./app/Filament/**/*.php",
-        "./resources/views/filament/**/*.blade.php",
-        "./vendor/filament/**/*.blade.php",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.tsx",
+    "./resources/views/**/*.blade.php",
+    "./app/Filament/**/*.php",
+    "./resources/views/filament/**/*.blade.php",
+    "./vendor/filament/**/*.blade.php",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "forest-green": "#5C6124",
+        "light-gray": "#D9D9D9",
+        "olive-gray": "#797A6D",
+      },
     },
+  },
 
-    plugins: [forms, typography],
+  plugins: [forms, typography],
 };
