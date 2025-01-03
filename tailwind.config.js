7d7800a85465e1780d91e02993258ffdccddda3b
Syawal Achmad <syawalachmad1323@gmail.com>
2025-01-03 20:06:52 +0800

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -41,5 +42,5 @@ export default {
     },
   },
 
-  plugins: [forms, typography],
+  plugins: [forms, typography, daisyui],
 };
