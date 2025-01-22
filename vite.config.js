887d319d31bbd3a7307584c48ede3990e4f10144
Syawal Achmad <syawalachmad1323@gmail.com>
2025-01-22 23:50:37 +0800

    feat: dockerize system for scalable deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    hmr: {
+      host: "0.0.0.0",
+    },
+  },
   plugins: [
     laravel({
       input: ["resources/css/app.css", "resources/js/app.tsx", "resources/css/filament/admin/theme.css"],
