41d8c6bf0a0767d4debc25938b638dd2df34a097
SywlAchmd <syawalachmad1323@gmail.com>
2025-04-10 14:54:31 +0800

    fix: update image layout for consistent aspect ratio

--- resources/js/components/pages/CardName.tsx
+++ resources/js/components/pages/CardName.tsx
@@ -4,14 +4,14 @@ import { getStaffDisplayName } from "@/utils/staffOptions";
 
 export default function CardName({ name, staff, image }: User) {
   return (
-    <section className="w-fit rounded-md bg-white p-2 shadow-xl">
+    <section className="max-w-[250px] rounded-md p-2 shadow-xl">
       <img
         alt={name}
         src={typeof image === "string" ? getStorageUrl(image) : getAssetUrl("images/default_avatar.jpg")}
-        className="aspect-square h-auto max-w-[250px] object-cover shadow-md smdlg:w-full"
+        className="aspect-square h-auto w-full object-cover shadow-md"
       />
 
-      <section className="py-4 text-center text-lg sm:text-xs mdlg:text-base">
+      <section className="flex h-[120px] flex-col justify-center py-4 text-center text-lg sm:gap-1 sm:text-xs mdlg:text-base">
         <p className="font-semibold text-forest-green">{name}</p>
         <p>{getStaffDisplayName(staff ?? "")}</p>
       </section>
